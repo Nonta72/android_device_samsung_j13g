631508b80ccd2a439a05e14ee6b890a63eca73eb
Coulibaly Nonta <coulibalynonta@gmail.com>
2016-09-17 15:39:11 +0100

    Fixed for J2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,19 +14,19 @@ TARGET_CPU_SMP                               := true
 ARCH_ARM_HAVE_TLS_REGISTER                   := true
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE                     := j13g,j13gxx,SM-J100H
+TARGET_OTA_ASSERT_DEVICE                     := j23g,j23dd,SM-J200H
 
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME                 := sc7727s
+TARGET_BOOTLOADER_BOARD_NAME                 := SC7730SE
 TARGET_NO_BOOTLOADER                         := true
 TARGET_NO_RADIOIMAGE                         := true
 
 # Kernel
-TARGET_PREBUILT_KERNEL                       := device/samsung/j13g/kernel
+TARGET_PREBUILT_KERNEL                       := device/samsung/j23g/kernel
 BOARD_KERNEL_CMDLINE                         := console=ttyS1,115200n8
 BOARD_KERNEL_BASE                            := 0x00000000
 BOARD_KERNEL_PAGESIZE                        := 2048
-BOARD_MKBOOTIMG_ARGS                         := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j13g/dt.img
+BOARD_MKBOOTIMG_ARGS                         := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j23g/dt.img
 
 # Fix this up by examining /proc/partitions on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE               := 0x105c0000
@@ -37,9 +37,9 @@ BOARD_FLASH_BLOCK_SIZE                       := 131072
 
 # TWRP Recovery
 BOARD_HAS_NO_SELECT_BUTTON                   := true
-TARGET_PREBUILT_KERNEL                       := device/samsung/j13g/kernel
-TARGET_RECOVERY_INITRC                       := device/samsung/j13g/init.rc
-TARGET_RECOVERY_FSTAB                        := device/samsung/j13g/twrp.fstab
+TARGET_PREBUILT_KERNEL                       := device/samsung/j23g/kernel
+TARGET_RECOVERY_INITRC                       := device/samsung/j23g/init.rc
+TARGET_RECOVERY_FSTAB                        := device/samsung/j23g/twrp.fstab
 BOARD_HAS_LARGE_FILESYSTEM                   := true
 TARGET_RECOVERY_PIXEL_FORMAT                 := "RGBX_8888"
 TW_THEME                                     := portrait_mdpi
