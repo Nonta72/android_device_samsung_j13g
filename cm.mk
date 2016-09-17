295d88bcdbd2683519bf4c52adf895461afa52a0
Coulibaly Nonta <coulibalynonta@gmail.com>
2016-09-17 15:40:52 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,17 +2,17 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := j13g
+PRODUCT_RELEASE_NAME := j23g
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/j13g/device_j13g.mk)
+$(call inherit-product, device/samsung/j23g/device_j23g.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := j13g
-PRODUCT_NAME := cm_j13g
+PRODUCT_DEVICE := j23g
+PRODUCT_NAME := cm_j23g
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := j13g
+PRODUCT_MODEL := j23g
 PRODUCT_MANUFACTURER := samsung
